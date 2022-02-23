f10a2c7199c09e68efb05fda4c84ac73094f8e88
paolchol <paolocolombo1996@gmail.com>
2022-02-23 18:25:37 +0100

    folder organization

---
+++ main.py
@@ -0,0 +1,4 @@
+#una volta finito il codice, scrivere bene qui dentro i comandi necessari
+
+#lasciare qui solo delle sezioni che una volta lanciate si colleghino ad altri script/moduli nella
+#cartella Python
