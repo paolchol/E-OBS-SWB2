af39d205a347bca6685c4a824201cc6f8b59e1c5
Claudia Medina <118851256+claumedimont@users.noreply.github.com>
2024-03-19 17:31:10 +0100

    All needed adjusts to calculate total recharge

--- MODEL-MI_2024/nc_outputSWB2.py
+++ MODEL-MI_2024/nc_outputSWB2.py
@@ -1,14 +1,13 @@
 """
-Process SWB2 output files
+Get sum of infiltration over stress periods from SWB2 output file
 Based on Guide_SWB2output.py by @paolochol
 
 Input files (output from SWB2):
-- net_infiltration
+- net_infiltration.nc
 
 Class(es):
 - EOBS-SWB2/Python/SWB2output.py
 
-#
 """
 
 import os
@@ -17,19 +16,18 @@ import numpy as np
 import pandas as pd
 
 #Call the class
-os.chdir('C:/Users/HP/Documents/GitHub/E-OBS-SWB2')
+os.chdir('c:/Users/user/OneDrive - Politecnico di Milano/GitHub/E-OBS-SWB2')
 from Python.SWB2output import SWB2output
 
 #Path to net infiltration netCDF file produced by SWB2
-swb2path = "C:/Users/HP/OneDrive - Politecnico di Milano/SWB2/MODEL-MI/output/R1_net_infiltration__2019-01-01_2022-12-31__338_by_660.nc"
+swb2path = "C:/Users/user/OneDrive - Politecnico di Milano/SWB2/MODEL-MI/output/R1_net_infiltration__2019-01-01_2022-12-31__338_by_660.nc"
 
 #Check the file
-nc_file = nc.Dataset(swb2path)
+#nc_file = nc.Dataset(swb2path)
 
 #Create the object
 f = SWB2output(swb2path)
 f.metadata #print all the metadata available
-print(f.metadata['end_date']) #print one variable of the metadata
 
 #Define stress periods, sum over them and assign sum to a new variable
 #Stress period definition
@@ -38,11 +36,10 @@ SP2 = 76   #days, 01/04 - 15/06
 SP3 = 92   #days, 16/06 - 15/09
 SP4 = 107  #days, 16/09 - 31/12
 SPs = [SP1, SP2, SP3, SP4]
-SPs = np.cumsum(SPs)
-SPsum = f.SP_sum(SPs)
+#SPs = np.cumsum(SPs)   #not sure if this is needed
+
+#Assign sum to a new variable and save ASCII files
+SPsum = f.SP_sum(SPs, outpath = 'c:/Users/user/OneDrive - Politecnico di Milano/SWB2/DataPreparation/SWB2_output')
 
 f.close()
 
-#Obtain a single dataframe with index, row, column, SP columns
-f.obtain_df_SP(index = 'indicatore')
-dfSP = f.results['dfSP']
